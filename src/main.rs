31f305eb9e940e637f42a80433c874cec2f1e6f6
Joe Mafrici <mac@joemafrici.com>
2024-08-29 11:32:05 -0700

    server receives connections

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use std::env;
+use std::io::{prelude::*, BufReader};
+use std::net::{TcpListener, TcpStream};
 fn main() -> std::io::Result<()> {
     let args: Vec<String> = env::args().collect();
     let file_path = &args[1];
@@ -14,5 +16,23 @@ fn main() -> std::io::Result<()> {
             }
         }
     }
+
+    let listener = TcpListener::bind("127.0.0.1:7878")?;
+    println!("server listening on port 7878...");
+    for stream in listener.incoming() {
+        handle_client(stream?);
+    }
+
     Ok(())
+}
+fn handle_client(mut stream: TcpStream) {
+    println!("connected to a client");
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    println!("Request: {http_request:#?}");
 }
