679fe93fecb8de4ed38108851f5082ed6341566e
Joe Mafrici <mac@joemafrici.com>
2024-09-01 21:22:08 -0700

    add http2 support feature for hyper

--- src/main.rs
+++ src/main.rs
@@ -96,6 +96,6 @@ fn rustls_server_config(key: impl AsRef<Path>, cert: impl AsRef<Path>) -> Arc<Se
         )
         .expect("bad certificate");
 
-    config.alpn_protocols = vec![b"h2".to_vec(), b"http/2".to_vec()];
+    config.alpn_protocols = vec![b"h2".to_vec(), b"http/1.1".to_vec()];
     Arc::new(config)
 }
